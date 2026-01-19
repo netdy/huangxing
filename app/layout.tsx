0b323d6a8fc2e58b507064645fcb8efcd890aecc
netdy <netdy@odds.team>
2026-01-19 13:14:00 +0700

    disable overflow-x-hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${notoSansSC.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${notoSansSC.variable} antialiased overflow-x-hidden`}
       >
         {children}
       </body>
